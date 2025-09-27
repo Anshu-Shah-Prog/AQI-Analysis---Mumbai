37e82cea69a85333d01cc36f3c3b3a51a8d5b932
Anshu-Shah-Prog <anshshah7643@gmail.com>
2025-09-28 00:29:43 +0530

    Update 04-models.py

--- notebooks/04-models.py
+++ notebooks/04-models.py
@@ -1,4 +1,4 @@
-from src.forecasting import run_arima, run_sarima, run_prophet, run_lstm
+from src.forecasting import run_sarima, run_prophet, run_lstm
 
 import pandas as pd
 df = pd.read_csv("data/processed/aqi.csv")
@@ -7,7 +7,6 @@ df = df.set_index("Date")
 ts = df["AQI"].asfreq("D")
 
 # Forecast models
-arima_result = run_arima(ts)
 sarima_result = run_sarima(ts)
 prophet_result = run_prophet(ts)
 lstm_result = run_lstm(ts)
